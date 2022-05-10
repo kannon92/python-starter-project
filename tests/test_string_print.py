4a795aa460e8098baaa8b9392da8fdd0fdd42431
Kevin Hannon <kevinhannon@macbook-pro.myfiosgateway.com>
2022-05-10 18:07:01 -0400

    Simple python project with tests, black, linting

---
+++ tests/test_string_print.py
@@ -0,0 +1,12 @@
+"""Simple test for hello"""
+from python_starter.string_print import string_print
+
+
+def test_hello():
+    '''Test of happy path with string'''
+    assert string_print("hello") == "hello"
+
+def test_hello_int():
+    '''Test of value with integer 5'''
+    value = 5
+    assert string_print(value) == 5
